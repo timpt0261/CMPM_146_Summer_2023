d8aa6312ef85d5fc20c1fb06bcf5d3fd2b4e1c20
Reuben Chavez <rchavez0215@gmail.com>
2023-07-13 17:22:49 -0700

    Added note in bt_bot

--- P3/behavior_tree_bot/bt_bot.py
+++ P3/behavior_tree_bot/bt_bot.py
@@ -2,21 +2,31 @@
 #
 
 """
-// There is already a basic strategy in place here. You can use it as a
-// starting point, or you can throw it out entirely and replace it with your
-// own.
+There is already a basic strategy in place here. You can use it as a
+starting point, or you can throw it out entirely and replace it with your
+own.
 """
-import logging, traceback, sys, os, inspect
-logging.basicConfig(filename=__file__[:-3] +'.log', filemode='w', level=logging.DEBUG)
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+import logging
+import traceback
+import sys
+import os
+import inspect
+logging.basicConfig(
+    filename=__file__[:-3] + '.log', filemode='w', level=logging.DEBUG)
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from behavior_tree_bot.behaviors import *
-from behavior_tree_bot.checks import *
+"""
+Make sure imports from local files are always below imports from libraries.
+This will make it hard to debug.
+"""
+from planet_wars import PlanetWars, finish_turn # this shouldn't be above import 
 from behavior_tree_bot.bt_nodes import Selector, Sequence, Action, Check
-
-from planet_wars import PlanetWars, finish_turn
+from behavior_tree_bot.checks import *
+from behavior_tree_bot.behaviors import *
 
 
 # You have to improve this tree or create an entire new one that is capable
@@ -42,11 +52,15 @@ def setup_behavior_tree():
     return root
 
 # You don't need to change this function
+
+
 def do_turn(state):
     behavior_tree.execute(planet_wars)
 
+
 if __name__ == '__main__':
-    logging.basicConfig(filename=__file__[:-3] + '.log', filemode='w', level=logging.DEBUG)
+    logging.basicConfig(
+        filename=__file__[:-3] + '.log', filemode='w', level=logging.DEBUG)
 
     behavior_tree = setup_behavior_tree()
     try:
